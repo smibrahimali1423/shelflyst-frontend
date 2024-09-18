37b3b80fd42dd49b153eeca53fe81639d7f3a372
Syed Mohammed Ibrahim Ali <sm.ibrahimali1423@gmail.com>
2024-09-18 20:51:22 +0530

    Update signUpThunks.js 2

--- src/redux/signUpThunks.js
+++ src/redux/signUpThunks.js
@@ -1,6 +1,5 @@
 import { signupStart, signupSuccess, signupFailure } from './authSlice';
-const host = "https://vercel.com/%E2%80%A6himali1423s-projects/shelflyst-backend-vercel";
-
+const host = "http://localhost:4000"
 export const signup = (name, email, password) => async (dispatch) => {
   dispatch(signupStart());
   try {
