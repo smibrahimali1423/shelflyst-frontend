2d93235cfc2d811107de33e6465a208b010b0c49
Syed Mohammed Ibrahim Ali <sm.ibrahimali1423@gmail.com>
2024-09-19 12:02:39 +0530

    Update signUpThunks.js m

--- src/redux/signUpThunks.js
+++ src/redux/signUpThunks.js
@@ -1,5 +1,5 @@
 import { signupStart, signupSuccess, signupFailure } from './authSlice';
-const host = "http://localhost:4000"
+const host = "https://shelflyst-backend-2.onrender.com"
 export const signup = (name, email, password) => async (dispatch) => {
   dispatch(signupStart());
   try {
