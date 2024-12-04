707baa1da7d7f2e5062d466a2ad3ec87cd2a7c90
Syed Mohammed Ibrahim Ali <sm.ibrahimali1423@gmail.com>
2024-12-04 12:28:36 +0530

    Update deleteUserThunk.js

--- src/redux/deleteUserThunk.js
+++ src/redux/deleteUserThunk.js
@@ -1,7 +1,7 @@
 import { deleteUserStart, deleteUserSuccess, deleteUserFailure } from './authSlice';
 import { deleteAllBooks } from './booksSlice'; // Adjust the path as needed
 
-const host = "http://localhost:4000"
+const host = "https://shelflyst-backend-2.onrender.com" 
 export const deleteUser = () => async (dispatch) => {
   dispatch(deleteUserStart());
   try {
